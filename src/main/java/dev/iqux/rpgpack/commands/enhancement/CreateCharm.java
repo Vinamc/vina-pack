3ed3402d8ba477c7799dcfc4ed1d70785a382a38
Alex Jim <boytunghc@gmail.com>
2020-06-04 18:22:20 +0700

    update charm class

--- src/main/java/dev/iqux/rpgpack/commands/enhancement/CreateCharm.java
+++ src/main/java/dev/iqux/rpgpack/commands/enhancement/CreateCharm.java
@@ -3,13 +3,42 @@ package dev.iqux.rpgpack.commands.enhancement;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import dev.iqux.rpgpack.Enhancement;
+import dev.iqux.rpgpack.utils.Config;
+import dev.iqux.rpgpack.utils.User;
 
 public class CreateCharm implements CommandExecutor {
 
+    public CreateCharm(JavaPlugin plugin) {
+        plugin.getCommand("setcharm").setExecutor(this);
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        // TODO Auto-generated method stub
-        return false;
+        if (! (sender instanceof Player)) {
+            sender.sendMessage("Can't excute this command in console");
+            return false;
+        }
+
+        Player player   = (Player) sender;
+        ItemStack charm = User.getPlayerHandItem(player);
+
+        if (! (player.hasPermission("rpgpack.admin.createcharm"))) {
+            player.sendMessage(Config.message("missing_permission"));
+            return false;
+        }
+
+        if (charm == null) {
+            player.sendMessage(Config.message("hand_empty"));
+            return false;
+        }
+
+        player.getInventory().setItemInMainHand(Enhancement.setCharm(charm));
+
+        return true;
     }
-    
 }
