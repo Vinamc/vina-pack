f3dc5d062ce1d4755d150660b35adef6ad85b39e
Alex Jim <boytunghc@gmail.com>
2020-06-05 12:55:07 +0700

    add play sound method

--- src/main/java/dev/iqux/rpgpack/utils/User.java
+++ src/main/java/dev/iqux/rpgpack/utils/User.java
@@ -1,6 +1,7 @@
 package dev.iqux.rpgpack.utils;
 
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -14,5 +15,22 @@ public class User {
         }
 
         return item;
+    }
+
+    public static void giveBackItem(Player p, ItemStack item) {
+
+        if (Utils.isAirItem(item)) {
+            return;
+        }
+
+        if (p.getInventory().firstEmpty() != -1) {
+            p.getInventory().addItem(item);
+        } else {
+            p.getWorld().dropItem(p.getLocation(), item);
+        }
+    }
+
+    public static void playSound(Player p, Sound sound) {
+        p.getWorld().playSound(p.getLocation(), sound, 5, 1);
     }
 }
