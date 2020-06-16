7f8bff45f7611984e4f3a9a211e9fbbee368d29a
Alex Jim <boytunghc@gmail.com>
2020-06-16 21:30:21 +0700

    remove most feature

--- src/main/java/dev/iqux/vina/App.java
+++ src/main/java/dev/iqux/vina/App.java
@@ -2,9 +2,6 @@ package dev.iqux.vina;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import dev.iqux.vina.commands.enhancement.*;
-import dev.iqux.vina.listeners.InventoryClick;
-import dev.iqux.vina.listeners.InventoryClose;
 import dev.iqux.vina.utils.Plugin;
 
 /**
@@ -20,9 +17,6 @@ public class App extends JavaPlugin
 
         this.bindPluginStatic();
 
-        this.bindCommands();
-        this.bindEvents();
-
         getLogger().info("Enable RPGBag Success");
     }
 
@@ -31,17 +25,6 @@ public class App extends JavaPlugin
         getLogger().info("See you again, SpigotMC!");
     }
 
-    private void bindCommands() {
-        new CreatePowder(this);
-        new CreateStone(this);
-        new ShowEnhance(this);
-        new CreateCharm(this);
-    }
-
-    private void bindEvents() {
-        new InventoryClick();
-        new InventoryClose();
-    }
 
     private void bindPluginStatic() {
         Plugin.bind(this);
