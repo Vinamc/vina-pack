516a462d50238395806b32671c5faabc24c7f027
Alex Jim <boytunghc@gmail.com>
2020-06-18 18:31:24 +0700

    add set stats

---
+++ src/main/java/dev/iqux/vina/services/commands/enchance/SetBasicArmor.java
@@ -0,0 +1,45 @@
+package dev.iqux.vina.services.commands.enchance;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import dev.iqux.vina.app.enchance.Armor;
+import dev.iqux.vina.utils.Config;
+import dev.iqux.vina.utils.User;
+
+public class SetBasicArmor extends SetCommand {
+    public static final String name = "setbasicarmor";
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (! this.validateSender(sender)) {
+            return false;
+        }
+
+        if (args.length < 1) {
+            this.alertInvalidCommand(sender);
+            return false;
+        }
+
+        Double stats = Double.parseDouble(args[0]);
+
+        if (stats < 0) {
+            Config.message("number_must_positive");
+            return false;
+        }
+
+        Player p = (Player) sender;
+
+        p.getInventory().setItemInMainHand(
+            Armor.setArmor(User.getPlayerHandItem(p), stats, 0.0)
+        );
+
+        return false;
+    }
+
+    @Override
+    protected String permission() {
+        return "vina.command.enchance.setstone";
+    }
+}
---
+++ src/main/java/dev/iqux/vina/services/commands/enchance/SetBasicDamage.java
@@ -0,0 +1,46 @@
+package dev.iqux.vina.services.commands.enchance;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import dev.iqux.vina.app.enchance.Weapon;
+import dev.iqux.vina.utils.Config;
+import dev.iqux.vina.utils.User;
+
+public class SetBasicDamage extends SetCommand {
+
+    public static final String name = "setbasicdamage";
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (! this.validateSender(sender)) {
+            return false;
+        }
+
+        if (args.length < 1) {
+            this.alertInvalidCommand(sender);
+            return false;
+        }
+
+        Double stats = Double.parseDouble(args[0]);
+
+        if (stats < 0) {
+            Config.message("number_must_positive");
+            return false;
+        }
+
+        Player p = (Player) sender;
+
+        p.getInventory().setItemInMainHand(
+            Weapon.setDamage(User.getPlayerHandItem(p), stats, 0.0)
+        );
+
+        return false;
+    }
+
+    @Override
+    protected String permission() {
+        return "vina.command.enchance.setstone";
+    }
+}
