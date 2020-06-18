45351b8f158f51107848bced84618cca18a56f37
Alex Jim <boytunghc@gmail.com>
2020-06-18 18:29:21 +0700

    fix check item

--- src/main/java/dev/iqux/vina/services/commands/enchance/SetCommand.java
+++ src/main/java/dev/iqux/vina/services/commands/enchance/SetCommand.java
@@ -6,9 +6,11 @@ import org.bukkit.inventory.ItemStack;
 
 import dev.iqux.vina.services.commands.BaseCommand;
 import dev.iqux.vina.utils.Config;
+import dev.iqux.vina.utils.Utils;
 
 abstract public class SetCommand extends BaseCommand {
 
+    @Override
     protected boolean validateSender(CommandSender sender) {
         if (! super.validateSender(sender)) {
             return false;
@@ -18,7 +20,7 @@ abstract public class SetCommand extends BaseCommand {
 
         ItemStack item = p.getInventory().getItemInMainHand();
 
-        if (item == null) {
+        if (Utils.isAirItem(item)) {
             p.sendMessage(Config.message("hand_empty"));
             return false;
         }
