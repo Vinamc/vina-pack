ea2ff7612b778bc2cc49b88edd6d938184e0aaaf
Alex Jim <boytunghc@gmail.com>
2020-06-18 18:27:50 +0700

    add command show enhance

---
+++ src/main/java/dev/iqux/vina/services/commands/enchance/ShowEnhance.java
@@ -0,0 +1,29 @@
+package dev.iqux.vina.services.commands.enchance;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import dev.iqux.vina.app.enchance.EnhancementUI;
+import dev.iqux.vina.services.commands.BaseCommand;
+
+public class ShowEnhance extends BaseCommand {
+
+    public static final String name = "showenhance";
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!this.validateSender(sender)) {
+            return false;
+        }
+
+        ((Player) sender).openInventory(EnhancementUI.GUI());
+
+        return true;
+    }
+
+    @Override
+    protected String permission() {
+        return "vina.command.enchance.use";
+    }
+}
