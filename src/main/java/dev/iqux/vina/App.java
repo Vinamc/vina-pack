09e7a64d7af53be600de41383a2fc0b66c2e62db
Alex <41188285+alexzvn@users.noreply.github.com>
2020-10-16 12:41:28 +0000

    add some placeholder

--- src/main/java/dev/iqux/vina/App.java
+++ src/main/java/dev/iqux/vina/App.java
@@ -1,5 +1,9 @@
 package dev.iqux.vina;
 
+import java.text.DecimalFormat;
+
+import org.bukkit.Bukkit;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import dev.iqux.vina.services.commands.CommandHandler;
@@ -8,10 +12,10 @@ import dev.iqux.vina.services.listeners.InventoryClick;
 import dev.iqux.vina.services.listeners.InventoryClose;
 import dev.iqux.vina.utils.Plugin;
 
-/**
- * Hello world!
- *
- */
+import be.maximvdw.placeholderapi.PlaceholderAPI;
+import be.maximvdw.placeholderapi.PlaceholderReplacer;
+import be.maximvdw.placeholderapi.PlaceholderReplaceEvent;
+
 public class App extends JavaPlugin
 {
     public static String version = "2.0.7";
@@ -24,6 +28,7 @@ public class App extends JavaPlugin
         this.bindPluginStatic();
         this.registerCommands();
         this.registerListeners();
+        this.registerPlaceholder();
 
         getLogger().info("Enable RPGBag Success");
     }
@@ -50,5 +55,34 @@ public class App extends JavaPlugin
         new InventoryClick();
         new InventoryClose();
         new EntityDamageByEntity();
+    }
+
+    private void registerPlaceholder() {
+        if (! Bukkit.getPluginManager().isPluginEnabled("MVdWPlaceholderAPI")) return;
+
+        PlaceholderReplacer maxHealth = new PlaceholderReplacer(){
+            @Override
+            public String onPlaceholderReplace(PlaceholderReplaceEvent event) {
+
+                DecimalFormat formatter = new DecimalFormat("#,###");
+
+                return formatter.format(
+                    event.getPlayer().getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue()
+                );
+            }
+        };
+
+        PlaceholderReplacer health = new PlaceholderReplacer(){
+            @Override
+            public String onPlaceholderReplace(PlaceholderReplaceEvent event) {
+
+                DecimalFormat formatter = new DecimalFormat("#,###");
+
+                return formatter.format(event.getPlayer().getHealth());
+            }
+        };
+
+        PlaceholderAPI.registerPlaceholder(this, "player_max_health", maxHealth);
+        PlaceholderAPI.registerPlaceholder(this, "player_health", health);
     }
 }
